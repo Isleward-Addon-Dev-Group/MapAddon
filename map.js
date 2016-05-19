17a6719ed48e8bf2e6583393ead392e7768e52b4
YasRavenheart <krystof.riha@outlook.com>
2016-05-19 22:09:11 +0200

    Render map on a canvas instead of a <table>

--- map.js
+++ map.js
@@ -2,9 +2,10 @@ addons.register({
     init: function(events) {
         this.collisionMap;
         this.uiContainer = $('.ui-container');
-        this.uiMap = $('<table class="addon-uiMap"></table>')
+        this.uiMap = $('<canvas class="addon-uiMap"></canvas>')
             .appendTo(this.uiContainer);
         
+        this.uiMap.css("display", "none");
         events.on('onGetMap', this.onGetMap.bind(this));
         events.on('onKeyDown', this.onKeyDown.bind(this));
     },
@@ -20,43 +21,43 @@ addons.register({
             return;
         } else if (key == "tab") {
             this.drawMap(this.collisionMap);
-        }  
+        }
     },
     drawMap: function(collisionMap) {
         if (!collisionMap) {
             return;
         }
         
-        if (this.uiMap.html()) {
-            this.uiMap.empty();
+        if (this.uiMap.css("display") == "block") {
             this.uiContainer.css('background-color', 'transparent');
 			this.uiContainer.removeClass('blocking');
+            this.uiMap.css("display", "none");
             return;
+        } else {
+            this.uiMap.css("display", "block");
         }
         
         this.uiContainer.css('background-color', 'rgba(49, 33, 54, 0.5)');
 	    this.uiContainer.addClass('blocking');
         
+        var ctx = this.uiMap[0].getContext('2d');
+        ctx.clearRect(0, 0, this.uiMap[0].width, this.uiMap[0].height);
         for (i = 0; i < collisionMap.length; i++) {
-            var trEl = $('<tr></tr>')
-            
             for (j = 0; j <collisionMap[i].length; j++) {
                 if (collisionMap[j][i] == 1) {
-                    trEl.append('<td style="background-color: white;"></td>');
+                    ctx.fillStyle = "white";
                 } else {
-                    trEl.append('<td></td>');
+                    ctx.fillStyle = "black";
                 }
+                
+                ctx.fillRect(j, i, 1, 1);
             }
-            
-            this.uiMap.append(trEl);
         }
         
         this.uiMap.css({
             'position': "absolute",
-            'color': "white",
             'left': (this.uiContainer[0].clientWidth / 2) - ($('.addon-uiMap')[0].clientWidth / 2),
             'top': (this.uiContainer[0].clientHeight / 2) - ($('.addon-uiMap')[0].clientHeight / 2),
-            'background-color': "rgba(49, 33, 54, 0.498039)",
         });
     }
 });
